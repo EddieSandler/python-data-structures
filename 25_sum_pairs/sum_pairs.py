5dcb8723b41d334639bb76d9ed43581b2c605ac4
Eddie Sandler <ed.sandler@gmail.com>
2023-10-02 15:22:08 -0400

    ex25

---
+++ 25_sum_pairs/sum_pairs.py
@@ -0,0 +1,38 @@
+def sum_pairs(nums, goal):
+    """Return tuple of first pair of nums that sum to goal.
+
+    For example:
+
+        >>> sum_pairs([1, 2, 2, 10], 4)
+        (2, 2)
+
+    (4, 2) sum to 6, and come before (5, 1):
+
+        >>> sum_pairs([4, 2, 10, 5, 1], 6) # (4, 2)
+        (4, 2)
+
+    (4, 3) sum to 7, and finish before (5, 2):
+
+        >>> sum_pairs([5, 1, 4, 8, 3, 2], 7)
+        (4, 3)
+
+    No pairs sum to 100, so return empty tuple:
+
+        >>> sum_pairs([11, 20, 4, 2, 1, 5], 100)
+        ()
+    """
+    #add num to result
+    #subtract num from goal
+    # if remainder is in result , return  num and reminder
+    # once the number and its remainder are in the results return num,remainder as tuple
+
+
+    result =[]
+    for num in nums:
+        remainder = goal - num
+        if remainder in result:
+            return (remainder,num)
+        result.append(num)
+
+    return ()
+
