e931997d085c531aca7455acb8e0511c4d2f3d01
Eddie Sandler <ed.sandler@gmail.com>
2023-10-05 12:12:18 -0400

    ex37

---
+++ 37_sum_up_diagonals/sum_up_diagonals.py
@@ -0,0 +1,32 @@
+def sum_up_diagonals(matrix):
+    """Given a matrix [square list of lists], return sum of diagonals.
+
+    Sum of TL-to-BR diagonal along with BL-to-TR diagonal:
+
+        >>> m1 = [
+        ...     [1,   2],
+        ...     [30, 40],
+        ... ]
+        >>> sum_up_diagonals(m1)
+        73
+
+        >>> m2 = [
+        ...    [1, 2, 3],
+        ...    [4, 5, 6],
+        ...    [7, 8, 9],
+        ... ]
+        >>> sum_up_diagonals(m2)
+        30
+    """
+
+
+    columns= len(matrix[0])
+    d1=0
+    d2=0
+
+    for i in range(columns):
+        d1 += matrix[i][i]
+        d2 += matrix[columns-1-i][i]
+
+    return d1+d2
+
