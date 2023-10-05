8c2bd0d8a79c5d412205470f8b2ef68ddc983294
Eddie Sandler <ed.sandler@gmail.com>
2023-10-05 09:59:36 -0400

    ex35

---
+++ 35_two_oldest_ages/two_oldest_ages.py
@@ -0,0 +1,37 @@
+def two_oldest_ages(ages):
+    """Return two distinct oldest ages as tuple (second-oldest, oldest)..
+
+        >>> two_oldest_ages([1, 2, 10, 8])
+        (8, 10)
+
+        >>> two_oldest_ages([6, 1, 9, 10, 4])
+        (9, 10)
+
+    Even if more than one person has the same oldest age, this should return
+    two *distinct* oldest ages:
+
+        >>> two_oldest_ages([1, 5, 5, 2])
+        (2, 5)
+    """
+
+    # NOTE: don't worry about an optimized runtime here; it's fine if
+    # you have a runtime worse than O(n)
+
+    # NOTE: you can sort lists with lst.sort(), which works in place (mutates);
+    # you may find it helpful to research the `sorted(iter)` function, which
+    # can take *any* type of list-like-thing, and returns a new, sorted list
+    # from it.
+
+
+    return tuple(sorted(list(set(sorted(ages))))[-2::])
+
+    # sorted_ages=sorted(ages)
+
+    # no_dupes=set(sorted_ages)
+
+    # result=list(no_dupes)
+
+    # my_result=sorted(result)
+
+    # return tuple(my_result[-2::])
+
